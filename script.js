4f290ac1711b16bb21535efc4d21ce44ab403b97
Sauby <imsauby@gmail.com>
2024-04-14 21:01:33 +0530

    Styled and created all functionalities

--- script.js
+++ script.js
@@ -6,45 +6,72 @@ const popoverList = [...popoverTriggerList].map(
   (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
 );
 
-document.addEventListener("DOMContentLoaded", function() {
-    const grossIncome = document.getElementById("gross-income");
-    const extraIncome = document.getElementById("extra-income");
-    const ageGroup = document.getElementById("age-group"); // Corrected ID
-    const deduction = document.getElementById("deduction");
-    const submit = document.getElementById("submit");
-  
-    submit.addEventListener("submit", function(e) {
-      e.preventDefault();
-  
-      document.getElementById("total-income").textContent = getTotalTax() * 100000;
-    });
-  
-    function getTotalTax() {
-      let deductionOnAge = 0;
-  
-      switch(ageGroup.value){
-          case "A":
-              deductionOnAge = 0.3;
-              break;
-          case "B":
-              deductionOnAge = 0.4;
-              break;
-          case "C":
-              deductionOnAge = 0.1;
-              break;
-          default:
-              deductionOnAge = 0;
-      }
-  
-      if (parseFloat(grossIncome) <= 8) deductionOnAge = 1;
+document.addEventListener("DOMContentLoaded", function () {
+  const grossIncome = document.getElementById("gross-income");
+  const extraIncome = document.getElementById("extra-income");
+  const ageGroup = document.getElementById("age-group");
+  const deduction = document.getElementById("deduction");
+  const submit = document.getElementById("submit");
 
-      const gross = parseFloat(grossIncome.value) || 0;
-      const extras = parseFloat(extraIncome.value) || 0;
-      const ded =  parseFloat(deduction.value) || 0
+  let showOK = true;
 
-      console.log(gross, extras, ded)
-  
-      return (deductionOnAge * (gross + extras - ded));
-    }
+  grossIncome.addEventListener("input", function (event) {
+    if (!/^\d+$/.test(event.target.value))
+      grossIncome.classList.add("is-invalid");
+    else grossIncome.classList.remove("is-invalid");
   });
-  
+
+  extraIncome.addEventListener("input", function (event) {
+    if (!/^\d+$/.test(event.target.value))
+      extraIncome.classList.add("is-invalid");
+    else extraIncome.classList.remove("is-invalid");
+  });
+
+  deduction.addEventListener("input", function (event) {
+    if (!/^\d+$/.test(event.target.value))
+      deduction.classList.add("is-invalid");
+    else deduction.classList.remove("is-invalid");
+  });
+
+  submit.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (showOK) {
+      document.getElementById("modalBtn").textContent = "Submit";
+      document
+        .getElementById("modalBtn")
+        .setAttribute("data-bs-toggle", "modal");
+      showOK = false;
+      return;
+    }
+
+    document.getElementById("total-income").textContent =
+      getTotalTax() * 100000;
+  });
+
+  function getTotalTax() {
+    let deductionOnAge = 0;
+
+    switch (ageGroup.value) {
+      case "A":
+        deductionOnAge = 0.3;
+        break;
+      case "B":
+        deductionOnAge = 0.4;
+        break;
+      case "C":
+        deductionOnAge = 0.1;
+        break;
+      default:
+        deductionOnAge = 1;
+    }
+
+    if (parseFloat(grossIncome.value) <= 8) deductionOnAge = 1;
+
+    const gross = parseFloat(grossIncome.value) || 0;
+    const extras = parseFloat(extraIncome.value) || 0;
+    const ded = parseFloat(deduction.value) || 0;
+
+    return deductionOnAge * (gross + extras - ded);
+  }
+});
